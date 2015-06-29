14d2fbcdbaf5a75c60ad0d578635f2e0bfcb0d44
Jake Rosoman <jkroso@gmail.com>
2015-06-29 12:37:09 +1200

    update to 0.28.3

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,8 @@
-version = v"0.28.1"
+version = v"0.28.3"
 filename = "electron-v$version-$(lowercase(string(OS_NAME)))-x$WORD_SIZE.zip"
 url = "https://github.com/atom/electron/releases/download/v$version/$filename"
 ispath(filename) || download(url, filename)
 
-ispath("dist") || run(`unzip $filename -d dist`)
+# Using external program because Julia's unzip package doesn't provide enough
+# metadata about each file to extract them correctly
+run(`unzip -ou $filename -d dist`)
